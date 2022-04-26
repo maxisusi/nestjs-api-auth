f628596ea945a7774cfa53a863cb57e5aabbe881
Max Balej <65896178+maxisusi@users.noreply.github.com>
2022-04-26 10:43:37 +0200

    Adding uniquer user validation

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
@@ -9,21 +10,38 @@ export class AuthService {
     // * Generate the password
     const hash = await argon.hash(dto.password);
     // * Save the new user in the db
-    const user = await this.prisma.user.create({
-      data: {
-        email: dto.email,
-        hash,
-      },
-    });
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
 
-    delete user.hash;
+      delete user.hash;
 
-    // * Return the user
+      // * Return the user
 
-    return user;
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials taken');
+        }
+      }
+
+      throw error;
+    }
   }
 
   signin() {
+    // * find ther user by email
+    // * if user doesn not exist throw exception
+
+    // * compare password
+    // * if password incorrect throw exception
+
+    // * send back user
     return { msg: 'Sign in Service' };
   }
 }
